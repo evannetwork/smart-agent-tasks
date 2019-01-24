fdc4fe60e85c9e0e642f75737b364ba427a0c3f2
wulfraem <wulfraem@users.noreply.github.com>
2019-01-24 14:20:37 +0100

    use `eslint-config-standard` (standardjs style) module for validating code style

--- config/smart-agent-tasks.js
+++ config/smart-agent-tasks.js
@@ -1,42 +1,40 @@
 /*
-  Copyright (C) 2018-present evan GmbH. 
-  
+  Copyright (C) 2018-present evan GmbH.
+
   This program is free software: you can redistribute it and/or modify it
-  under the terms of the GNU Affero General Public License, version 3, 
-  as published by the Free Software Foundation. 
-  
-  This program is distributed in the hope that it will be useful, 
-  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  under the terms of the GNU Affero General Public License, version 3,
+  as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-  See the GNU Affero General Public License for more details. 
-  
+  See the GNU Affero General Public License for more details.
+
   You should have received a copy of the GNU Affero General Public License along with this program.
   If not, see http://www.gnu.org/licenses/ or write to the
-  
+
   Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA, 02110-1301 USA,
-  
-  or download the license from the following URL: https://evan.network/license/ 
-  
+
+  or download the license from the following URL: https://evan.network/license/
+
   You can be released from the requirements of the GNU Affero General Public License
   by purchasing a commercial license.
   Buying such a license is mandatory as soon as you use this software or parts of it
-  on other blockchains than evan.network. 
-  
-  For more information, please contact evan GmbH at this address: https://evan.network/license/ 
+  on other blockchains than evan.network.
 
-  
+  For more information, please contact evan GmbH at this address: https://evan.network/license/
 */
 
 exports['default'] = {
-  
+
   smartAgentTasks: (api) => {
     return {
       disabled: false,
       name: 'smartAgentTasks',
       // local test account
-      //ethAccount: '0xa60F5EAfBb782793d7589bc5F55BfA3a599B182d',
+      // ethAccount: '0xa60F5EAfBb782793d7589bc5F55BfA3a599B182d',
       // live account
-      ethAccount: '0x00B7BD965DB2c7D666Fdf155a69FF66ea8ae1320',
+      ethAccount: '0x00B7BD965DB2c7D666Fdf155a69FF66ea8ae1320'
     }
   }
 }
--- initializers/smart-agent-tasks.js
+++ initializers/smart-agent-tasks.js
@@ -1,43 +1,40 @@
 /*
-  Copyright (C) 2018-present evan GmbH. 
-  
+  Copyright (C) 2018-present evan GmbH.
+
   This program is free software: you can redistribute it and/or modify it
-  under the terms of the GNU Affero General Public License, version 3, 
-  as published by the Free Software Foundation. 
-  
-  This program is distributed in the hope that it will be useful, 
-  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  under the terms of the GNU Affero General Public License, version 3,
+  as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-  See the GNU Affero General Public License for more details. 
-  
+  See the GNU Affero General Public License for more details.
+
   You should have received a copy of the GNU Affero General Public License along with this program.
   If not, see http://www.gnu.org/licenses/ or write to the
-  
+
   Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA, 02110-1301 USA,
-  
-  or download the license from the following URL: https://evan.network/license/ 
-  
+
+  or download the license from the following URL: https://evan.network/license/
+
   You can be released from the requirements of the GNU Affero General Public License
   by purchasing a commercial license.
   Buying such a license is mandatory as soon as you use this software or parts of it
-  on other blockchains than evan.network. 
-  
-  For more information, please contact evan GmbH at this address: https://evan.network/license/ 
+  on other blockchains than evan.network.
 
-  
+  For more information, please contact evan GmbH at this address: https://evan.network/license/
 */
 
 'use strict'
 const request = require('request')
 const { Initializer, api } = require('actionhero')
-const { ContractState, } = require('@evan.network/api-blockchain-core')
+const { ContractState } = require('@evan.network/api-blockchain-core')
 
 const config = api.config.smartAgentTasks
 const weatherEn = /.*weather (?:in|at|near) ([\w -]+)/i
 const weatherDe = /.*wetter (?:in|bei|nahe|um) ([\w -]+)/i
 
-
-async function getWeatherComment(comment) {
+async function getWeatherComment (comment) {
   return new Promise((resolve, reject) => {
     let lang
     let units
@@ -50,11 +47,11 @@ async function getWeatherComment(comment) {
       location = comment.replace(weatherDe, '$1')
       lang = 'de'
       units = 'metric'
-    } else if (comment.indexOf('weather') != -1) {
+    } else if (comment.indexOf('weather') !== -1) {
       location = 'Eisenach'
       lang = 'en'
       units = 'imperial'
-    } else if (comment.indexOf('wetter') != -1) {
+    } else if (comment.indexOf('wetter') !== -1) {
       location = 'Eisenach'
       lang = 'de'
       units = 'metric'
@@ -67,19 +64,18 @@ async function getWeatherComment(comment) {
           reject(error)
         } else {
           const weather = JSON.parse(body)
-          if(!weather.name) {
+          if (!weather.name) {
             resolve(`${lang === 'de' ? 'Wetter konnte nicht ermittelt werden ' : 'weather could not be retrieved '}`)
           } else {
             resolve(`${lang === 'de' ? 'Wetter in ' : 'weather in '}${weather.name}: ${weather.main.temp}°${lang === 'de' ? 'C' : 'F'}, ${weather.weather[0].description}`)
           }
         }
-      });
+      })
     } else {
-      resolve(null);
+      resolve(null)
     }
-  });
+  })
 }
-
 
 module.exports = class SmartAgentTasks extends Initializer {
   constructor () {
@@ -95,26 +91,28 @@ module.exports = class SmartAgentTasks extends Initializer {
       return
     }
     class SmartAgentTasks extends api.smartAgents.SmartAgent {
-      async startTaskListener() {
+      async startTaskListener () {
         try {
           const taskContractType = api.eth.web3.utils.sha3('TaskDataContract')
           let processingQueue = Promise.resolve()
           // get block from last uptime
-          const lastBlockOuter  = await api.redis.clients.client.get('evannetwork:smartAgentTasks:lastBlock') || (await api.eth.web3.eth.getBlockNumber())
+          const lastBlockOuter = await api.redis.clients.client.get('evannetwork:smartAgentTasks:lastBlock') || (await api.eth.web3.eth.getBlockNumber())
           api.log(`last lastBlockOuter: ${lastBlockOuter}`)
           await this.runtime.eventHub.subscribe('EventHub', null, 'ContractEvent',
             async (event) => {
-              const { eventType, contractType, member } = event.returnValues;
-              return member === this.config.ethAccount &&   // invited user is smart agent
-                contractType === taskContractType &&        // task data contract
-                eventType === '0'                           // invite
-              ;
+              const { eventType, contractType, member } = event.returnValues
+              // invited user is smart agent
+              // task data contract
+              // invite
+              return member === this.config.ethAccount &&
+                contractType === taskContractType &&
+                eventType === '0'
             },
             (event) => {
               const handleEvent = async () => {
                 // we got an invitation, wait for a contract release
-                const blockNumber = event.blockNumber;
-                const { contractAddress } = event.returnValues;
+                const blockNumber = event.blockNumber
+                const { contractAddress } = event.returnValues
                 // mark outer block as handled
                 await api.redis.clients.client.set('evannetwork:smartAgentTasks:lastBlock', blockNumber)
                 // store block and contract to continue later on
@@ -130,25 +128,25 @@ module.exports = class SmartAgentTasks extends Initializer {
                 })
               return processingQueue
             },
-            lastBlockOuter,
+            lastBlockOuter
           )
           // load all remaining contracts and start listeners
           // fetch remaining contract listener and start their own once listener
           const remaining = (await api.redis.clients.client.zrange('evannetwork:smartAgentTasks:lastBlocks', 0, -1, 'WITHSCORES'))
           api.log(`remaining blocks and contracts: ${JSON.stringify(remaining, null, 2)}`)
           while (remaining.length >= 2) {
-            const [contractAddress, block] = remaining.splice(0, 2);
+            const [contractAddress, block] = remaining.splice(0, 2)
             this.subscribeForContractRelease(contractAddress, block)
           }
         } catch (ex) {
-          api.log(`could not bind taskListener; ${ ex.message || ex }`, 'warning')
+          api.log(`could not bind taskListener; ${ex.message || ex}`, 'warning')
         }
       }
-      async subscribeForContractRelease(contractAddress, block) {
+      async subscribeForContractRelease (contractAddress, block) {
         this.runtime.eventHub.once('BaseContractInterface', contractAddress, 'StateshiftEvent',
           (event) => {
-            const { state } = event.returnValues;
-            return state && parseInt(state, 10) === ContractState.Active;
+            const { state } = event.returnValues
+            return state && parseInt(state, 10) === ContractState.Active
           },
           async () => {
             // mark block/contract as done
@@ -157,7 +155,7 @@ module.exports = class SmartAgentTasks extends Initializer {
             const isConsumer = await this.runtime.executor.executeContractCall(contract, 'isConsumer', this.config.ethAccount)
             if (isConsumer) {
               const entries = await this.runtime.dataContract.getListEntries(contract, 'todos', this.config.ethAccount)
-              await this.runtime.dataContract.changeConsumerState(contract, this.config.ethAccount, this.config.ethAccount, 4);
+              await this.runtime.dataContract.changeConsumerState(contract, this.config.ethAccount, this.config.ethAccount, 4)
               if (entries && entries.length) {
                 for (let entry of entries) {
                   let answer = {
@@ -165,7 +163,7 @@ module.exports = class SmartAgentTasks extends Initializer {
                     comment: null,
                     solveTime: (new Date()).getTime(),
                     solver: this.config.ethAccount,
-                    solverAlias: 'Smart Agent',
+                    solverAlias: 'Smart Agent'
                   }
                   answer.comment = await getWeatherComment(entry.alias.toLowerCase())
                   if (answer.comment) {
@@ -175,7 +173,7 @@ module.exports = class SmartAgentTasks extends Initializer {
               }
             }
           }, block
-        );
+        )
       }
     }
 
